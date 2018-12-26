f14e83955088184f2d8a12209e564134c867ad6f
KDHW2918 <mathieu.cans@orange.com>
2018-12-26 18:15:26 +0100

    create surface area

--- app/src/main/java/com/cans/ponggame/PongActivity.java
+++ app/src/main/java/com/cans/ponggame/PongActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 
 public class PongActivity extends AppCompatActivity {
 
+    private PongView view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pong);
+
+        view = new PongView(this);
+
+        setContentView(view);
     }
 }
---
+++ app/src/main/java/com/cans/ponggame/PongView.java
@@ -0,0 +1,12 @@
+package com.cans.ponggame;
+
+import android.content.Context;
+import android.view.SurfaceView;
+import android.view.View;
+
+class PongView extends SurfaceView {
+
+    public PongView(Context context) {
+        super(context);
+    }
+}
