e7e7e362e42226270916eb08a0c234780a969924
KDHW2918 <mathieu.cans@orange.com>
2018-12-26 18:35:36 +0100

    draw green

--- app/src/main/java/com/cans/ponggame/PongView.java
+++ app/src/main/java/com/cans/ponggame/PongView.java
@@ -1,12 +1,50 @@
 package com.cans.ponggame;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
 
-class PongView extends SurfaceView {
+class PongView extends SurfaceView implements Runnable {
+
+    private final SurfaceHolder holder;
+    private final Thread drawingThread;
 
     public PongView(Context context) {
         super(context);
+
+        holder = getHolder();
+        drawingThread = new Thread(this);
+
+        drawingThread.start();
+    }
+
+
+    @Override
+    public void run()
+    {
+        while (!Thread.currentThread().isInterrupted())
+        {
+            draw();
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Log.i(VIEW_LOG_TAG,"sleep interrupt");
+            }
+        }
+    }
+
+    private void draw() {
+        if (holder.getSurface().isValid())
+        {
+            Canvas canvas = holder.lockCanvas();
+
+            canvas.drawColor(Color.argb(255, 120, 197, 87));
+
+            holder.unlockCanvasAndPost(canvas);
+        }
     }
 }
